052dd104bdfaff263a50e47ce5d537543e6e8f0c
Ömer Kaşdarma <omer.kasdarma@gmail.com>
2021-03-07 18:28:55 +0300

    some major changes and also implemented .clear() & .flush().

--- app.js
+++ app.js
@@ -1,35 +1,95 @@
 import AsyncQueue from "./lib/async-queue.js";
 
-var aq = new AsyncQueue();
+function makePromiseArray({length,maxDuration,rejectRatio}){
+  return Array.from({length}, (_,i) => new Promise((v,x) => {
+                                                     var dur = ~~(Math.random()*maxDuration),
+                                                         act = Math.random() < rejectRatio ? { fun: x
+                                                                                             , msg: { name   : "Bad Luck"
+                                                                                                    , message: `Promise ${i} expected at ${dur}ms but rejected due to ${rejectRatio*100}% bad luck`
+                                                                                                    }
+                                                                                             }
+                                                                                           : { fun: v
+                                                                                             , msg: `Promise ${i} resolved at ${dur}ms due to ${100*(1-rejectRatio)}% good luck`
+                                                                                             };
+                                                     setTimeout(act.fun,dur,act.msg);
+                                                   }));
+};
+
+var errh = e => console.log(`The error handler caught exception ${e.name} caught due to "${e.message}"`),
+    opts = { timeout  : 200
+           , errHandle: false //errh
+           , clearMode: "soft"
+           },
+    aq   = new AsyncQueue(opts),
+    clr  = 350,  // 1st clear time
+    rnd  = 300,  // 10 promises with 0 ~ rnd ms resolve
+    as1  = 375,  // asynchrnous instertion of a string
+    ap1  = 400,  // asynchrnous instertion of a promise
+    aa1  = 500,  // asynchrnous instertion of an array of promises
+    fls  = 650,  // flush at
+    aop  = { length     : 100
+           , maxDuration: 300
+           , rejectRatio: 0.005
+           };
+
+aq.clear();
 
 async function getAsyncValues(){
   for await (let item of aq){
-    console.log(`The Promise resolved with a value of ${item.value}`);
+    console.log(`The Promise at endpoint resolved with a value of "${item}"`);
   };
 };
 
 getAsyncValues();
 
 for (var i=1, r; i <= 10; i++){
-  r = Math.random()*5950;
+  r = ~~(Math.random()*rnd);
   aq.enqueue(new Promise(resolve => setTimeout(resolve, r, `done ${i} at ${r}ms`)));
-  };
+};
+
+setTimeout(_ => console.log(`Size at timeout (${opts.timeout}ms) is ${aq.size}`),opts.timeout);
 
 setTimeout(function(){
+             console.log(`Queue size just before clearing is ${aq.size}`);
              aq.clear();
-             console.log(`Queue cleared @ 5000ms ${aq.size ? "but there is still one more promise to resolve..!" : ""}`);
-           }, 5000);
+             console.log(`Queue cleared @${clr}ms ${aq.size ? "but previously resolved promises are kept..!" : "with no remaining items"}`);
+           }, clr);
 
 setTimeout(function(){
-             console.log("size @ 6000ms is:", aq.size);
-             aq.enqueue(Promise.resolve('"this was an asynchronous insertion"'));
-           }, 6000);
+             console.log(`size @ ${as1}ms is: ${aq.size}`);
+             aq.enqueue("Asynchronous insertion of this string");
+           }, as1);
 
 setTimeout(function(){
-             console.log("size @ 7000ms is:", aq.size);
-             aq.enqueue(Promise.resolve('"this was an asynchronous insertion"'));
-           }, 7000);
+             console.log(`size @ ${ap1}ms is: ${aq.size}`);
+             aq.enqueue(Promise.resolve("Asynchronous insertion of a promise"));
+           }, ap1);
 
-for (var i=1; i <= 2; i++) aq.enqueue(i);
-setTimeout(function(){console.log("Size:",aq.size);},7500);
+setTimeout(function(){console.log(`Size @${ap1+1}ms: ${aq.size}`);},ap1+1);
+
+setTimeout(function(){
+             makePromiseArray(aop).forEach(p => aq.enqueue(p));
+             console.log(`Queue size ${fls-aa1}ms before flush() is ${aq.size}`);
+           },aa1);
+
+setTimeout(function(){
+             var ps = aq.flush();
+             console.log(`Flushed ${ps.length} promises and the queue size after flush is ${aq.size}`);
+             Promise.all(ps)
+                    .then(rs => console.log("Promise.all() resolutions", rs))
+                    .catch(e => console.log(`Promise.all() caught ${e.name || e} exception: ${e.message || e}`));
+           },fls);
+
+setTimeout(function(){
+             console.log(`size @ ${aa1+250}ms is: ${aq.size}`);
+             aq.enqueue("Asynchronous insertion of this second string");
+           }, aa1+250);
+
+setTimeout(function(){
+             console.log(`size @ ${aa1+250}ms is: ${aq.size}`);
+             aq.enqueue(Promise.resolve("Asynchronous insertion of another promise"));
+           }, aa1+500);
+
+for (var i=1; i <= 2; i++) aq.enqueue("Synchronous " + i);
+
 
